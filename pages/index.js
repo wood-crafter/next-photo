f999d1cef787d0f29735a59a7bcfaeeb28ccdf94
hungpv <phanhung2702@gmail.com>
2024-05-20 18:51:05 +0700

    [Add] change image width

--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,31 @@ export default function Home() {
   return (
     <div className={styles.container} style={{display: 'flex', flexDirection: 'column'}}>
       <div style={{display: 'flex', width: '100%', justifyContent: 'center'}}>
-        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbDdxYzdoczN2ZjNzd3R3amM5MGozcTdkYnFoMGkwMW96OGEzbmNnYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gVsmn4qdyBn1Bra2tN/giphy.gif"  style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
-        <img src="https://media0.giphy.com/media/GwbVjTKRkFgqs/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
-        <img src="https://media1.giphy.com/media/yG69gK8LN0Bm5YLcrC/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
-        <img src="https://media3.giphy.com/media/DA8op0omzFuwe14iyj/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbDdxYzdoczN2ZjNzd3R3amM5MGozcTdkYnFoMGkwMW96OGEzbmNnYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gVsmn4qdyBn1Bra2tN/giphy.gif"  style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
+        <img src="https://media0.giphy.com/media/GwbVjTKRkFgqs/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
+        <img src="https://media1.giphy.com/media/yG69gK8LN0Bm5YLcrC/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
+        <img src="https://media3.giphy.com/media/DA8op0omzFuwe14iyj/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
       </div>
 
       <div style={{display: 'flex', width: '100%', justifyContent: 'center', alignItems: 'center'}}>
         <div style={{height: '100%', display: 'flex', flexDirection: 'column'}}>
           <h1>Sáp</h1>
-          <img src="https://media1.giphy.com/media/KIbxcYxk0KMGXkc10O/giphy.webp?cid=ecf05e47rbo9rx4tbdnjaguhfmr0enceqn9dgzrpqz72rwv7&ep=v1_gifs_search&rid=giphy.webp&ct=g" style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
+          <img src="https://media1.giphy.com/media/KIbxcYxk0KMGXkc10O/giphy.webp?cid=ecf05e47rbo9rx4tbdnjaguhfmr0enceqn9dgzrpqz72rwv7&ep=v1_gifs_search&rid=giphy.webp&ct=g" style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
           <h1>cố</h1>
         </div>
-        <img src="Remove-bg.ai_1715269161939.png" style={{width: '16rem', height: '20rem' }}/>
+        <img src="Remove-bg.ai_1715269161939.png" style={{width: '13rem', height: '20rem' }}/>
         <div style={{height: '100%', display: 'flex', flexDirection: 'column'}}>
           <h1>ơi</h1>
-          <img src="https://media3.giphy.com/media/PPgZCwZPKrLcw75EG1/200.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=200.webp&ct=g" style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
+          <img src="https://media3.giphy.com/media/PPgZCwZPKrLcw75EG1/200.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=200.webp&ct=g" style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
           <h1>lên</h1>
         </div>
       </div>
 
       <div style={{display: 'flex', width: '100%', justifyContent: 'center'}}>
-        <img src="https://media1.giphy.com/media/CnVSvvHyslUkM/200w.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=200w.webp&ct=g"  style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
-        <img src="https://media2.giphy.com/media/l2QDTsTL2NhwOEAV2/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
-        <img src="https://media1.giphy.com/media/hT9psZ0PCtmYvFAxnc/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
-        <img src="https://media0.giphy.com/media/3o752eDHQgwl8zS3Oo/200.webp?cid=ecf05e47rbo9rx4tbdnjaguhfmr0enceqn9dgzrpqz72rwv7&ep=v1_gifs_search&rid=200.webp&ct=g"  style={{marginRight: '2rem', width: '6rem', height: '6rem'}}/>
+        <img src="https://media1.giphy.com/media/CnVSvvHyslUkM/200w.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=200w.webp&ct=g"  style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
+        <img src="https://media2.giphy.com/media/l2QDTsTL2NhwOEAV2/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
+        <img src="https://media1.giphy.com/media/hT9psZ0PCtmYvFAxnc/giphy.webp?cid=790b7611xg3h0rv1ensw8rul45hoh650anpq4wy9nmt53c2l&ep=v1_gifs_search&rid=giphy.webp&ct=g"  style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
+        <img src="https://media0.giphy.com/media/3o752eDHQgwl8zS3Oo/200.webp?cid=ecf05e47rbo9rx4tbdnjaguhfmr0enceqn9dgzrpqz72rwv7&ep=v1_gifs_search&rid=200.webp&ct=g"  style={{marginRight: '2rem', width: '4.5rem', height: '4.5rem'}}/>
       </div>
       <style jsx>{`
         h1 {
